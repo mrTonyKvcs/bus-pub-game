0eaacb4fabb587c24900291b40f79b8790fe42da
tony <attila.kovacs92@gmail.com>
2020-03-22 16:35:24 +0100

    set up friendships

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,21 +3,17 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+        @foreach($users as $user)
+            @if (Auth::id() != $user->id)
+                <div class="mb-3 col-md-8">
+                    <div class="card">
+                        <div class="card-body">
+                            {{ $user->email }}
                         </div>
-                    @endif
-
-                    You are logged in!
+                    </div>
                 </div>
-            </div>
-        </div>
+                @endif
+        @endforeach
     </div>
 </div>
 @endsection
